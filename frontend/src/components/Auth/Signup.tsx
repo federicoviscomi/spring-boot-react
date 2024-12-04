0d415b245be6a1564cba7f46d0b87f899bdb8d3b
Federico Viscomi <federicoviscomi@gmail.com>
2024-12-04 16:03:33 +0000

    refactoring

--- frontend/src/components/Auth/Signup.tsx
+++ frontend/src/components/Auth/Signup.tsx
@@ -1,24 +1,26 @@
+import axios from "axios";
 import React, {useEffect, useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
-import api from "../../services/api";
-import {FcGoogle} from "react-icons/fc";
-import {FaGithub} from "react-icons/fa";
-import Divider from "@mui/material/Divider";
-import Button from "../common/Button";
-import InputField from "../common/InputField";
 import {useForm} from "react-hook-form";
 import toast from "react-hot-toast";
+
+import Divider from "@mui/material/Divider";
+
+import Button from "../common/Button";
+import InputField from "../common/InputField";
+import {FcGoogle} from "react-icons/fc";
+import {FaGithub} from "react-icons/fa";
 import {useMyContext} from "../../store/AppContext";
+import api from "../../services/api";
 
 const Signup = () => {
-    const apiUrl = process.env.REACT_APP_API_URL;
-    const [role, setRole] = useState();
-    const [loading, setLoading] = useState(false);
-    // Access the token and setToken function using the useMyContext hook from the AppContextProvider
     const {token} = useMyContext();
+
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const [role, setRole] = useState<string | undefined>(undefined);
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
-    //react hook form initialization
     const {
         register,
         handleSubmit,
@@ -38,7 +40,7 @@ const Signup = () => {
         setRole("ROLE_USER");
     }, []);
 
-    const onSubmitHandler = async (data) => {
+    const onSubmitHandler = async (data: any) => {
         const {username, email, password} = data;
         const sendData = {
             username,
@@ -50,30 +52,25 @@ const Signup = () => {
         try {
             setLoading(true);
             const response = await api.post("/auth/public/signup", sendData);
-            toast.success("Reagister Successful");
+            toast.success("Registered Successful");
             reset();
             if (response.data) {
                 navigate("/login");
             }
         } catch (error) {
-            // Add an error programmatically by using the setError function provided by react-hook-form
-            //setError(keyword,message) => keyword means the name of the field where I want to show the error
-
-            if (
-                error?.response?.data?.message === "Error: Username is already taken!"
-            ) {
-                setError("username", {message: "username is already taken"});
-            } else if (
-                error?.response?.data?.message === "Error: Email is already in use!"
-            ) {
-                setError("email", {message: "Email is already in use"});
+            if (error && axios.isAxiosError(error) && error.response?.data?.message) {
+                const errorMessage = error.response.data.message;
+                if (errorMessage === "Error: Username is already taken!") {
+                    setError("username", {message: "username is already taken"});
+                } else if (errorMessage === "Error: Email is already in use!") {
+                    setError("email", {message: "Email is already in use"});
+                }
             }
         } finally {
             setLoading(false);
         }
     };
 
-    //if there is token exist navigate to the user to the home page if he tried to access the login page
     useEffect(() => {
         if (token) navigate("/");
     }, [navigate, token]);
@@ -96,29 +93,27 @@ const Signup = () => {
                             href={`${apiUrl}/oauth2/authorization/google`}
                             className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
                         >
-              <span>
-                <FcGoogle className="text-2xl"/>
-              </span>
+                            <span>
+                                <FcGoogle className="text-2xl"/>
+                            </span>
                             <span className="font-semibold sm:text-customText text-xs">
-                Login with Google
-              </span>
+                                Login with Google
+                            </span>
                         </a>
                         <a
                             href={`${apiUrl}/oauth2/authorization/github`}
                             className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
                         >
-              <span>
-                <FaGithub className="text-2xl"/>
-              </span>
+                            <span>
+                                <FaGithub className="text-2xl"/>
+                            </span>
                             <span className="font-semibold sm:text-customText text-xs">
-                Login with Github
-              </span>
+                                Login with Github
+                            </span>
                         </a>
                     </div>
-
                     <Divider className="font-semibold">OR</Divider>
                 </div>
-
                 <div className="flex flex-col gap-2">
                     <InputField
                         label="UserName"
@@ -158,11 +153,10 @@ const Signup = () => {
                     onClickHandler={() => {
                     }}
                     className="bg-customRed font-semibold flex justify-center text-white w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3"
-                    type="text"
+                    type="submit"
                 >
                     {loading ? <span>Loading...</span> : "Register"}
                 </Button>
-
                 <p className="text-center text-sm text-slate-700 mt-2">
                     Already have an account?
                     <Link
