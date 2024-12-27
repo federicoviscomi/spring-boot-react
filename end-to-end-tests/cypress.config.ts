16f340d13c25f8213c3e948047853a73160260b0
Federico Viscomi <federicoviscomi@gmail.com>
2024-12-27 23:39:40 +0100

    asdf

--- end-to-end-tests/cypress.config.ts
+++ end-to-end-tests/cypress.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_baseUrl
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000'
   },
   env: {
-    HOME_PAGE: process.env.CYPRESS_baseUrl,
+    HOME_PAGE: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
     ADMIN_USER: "admin",
     ADMIN_PASS: "adminPass",
     ADMIN_EMAIL: "admin@example.com",
